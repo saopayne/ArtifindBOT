c3eb9a0791b28b3c87422768a6057649d4560770
Oyewale Ademola <saopayne@gmail.com>
2016-05-11 00:03:09 +0100

    updated the bot to speak

--- index.js
+++ index.js
@@ -32,3 +32,38 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "EAAY0SIK3SQ8BANUKxNcXFInZAWTO78CHhu5nvWKJlHb1m2MBoyhQImyrCwWM6eQ9XsXZB4shGLCekkxyiV7Ak4GcZClR5e9ZADB78o0GsIb80V2ZC9MCz1PPjgyIfqbzp0vx6F4HtWQBTZABMvHrMH5skTjfvTKuziztCuk8mJzAZDZD"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
